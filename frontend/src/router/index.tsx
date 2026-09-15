b710a5b6c70903e5ba773be2f9c073e68f8ed930
Ostap Kolosivskyi <ostap.kolosivskyy@gmail.com>
2026-09-15 20:44:24 +0300

    the first part on login on FE

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -2,20 +2,44 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage/index";
 import Profile from "../pages/Profile";
 import MyCars from "../pages/MyCars";
+import LoginPage from "../pages/Login";
+import { ProtectedRoute } from "./ProtectedRoute";
+import { PublicRoute } from "./PublicRoute";
+import RegisterPage from "../pages/Register";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
+    element: <PublicRoute />,
+    children: [
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+    ],
   },
+
   {
-    path: "/profile",
-    element: <Profile />,
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/mycars",
+        element: <MyCars />,
+      },
+    ],
   },
-  {
-    path: "/mycars",
-    element: <MyCars />,
-  },
+
   {
     path: "*",
     element: <div>404: Сторінку не знайдено</div>,
